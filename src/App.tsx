c481af2087870350abfd75acec82fb1651b768d6
Sarma <avnmsarma@gmail.com>
2024-04-12 09:59:02 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
